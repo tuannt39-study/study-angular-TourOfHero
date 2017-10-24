b04f9fafd737716c1486d623e2ea31efd3d27f8e
Nguyễn Thế Tuân <tuannt39@gmail.com>
2017-10-24 11:46:07 +0700

    part 2 - master, detail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,20 @@ export class Hero {
   name: string;
 }
 
+// Create heroes
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,8 +29,9 @@ export class Hero {
 // Show the hero
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES; // Expose heroes
+  selectedHero: Hero; // Add a click handler to expose the selected hero
+  onSelect(hero:Hero): void{
+    this.selectedHero = hero;
+  }
 }
