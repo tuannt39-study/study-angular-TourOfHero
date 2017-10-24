42145cda1059b146d361fb141d7a4a60dbce5704
Nguyễn Thế Tuân <tuannt39@gmail.com>
2017-10-24 11:02:24 +0700

    part 1 - hero editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+// Hero object
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+// Show the hero
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 }
